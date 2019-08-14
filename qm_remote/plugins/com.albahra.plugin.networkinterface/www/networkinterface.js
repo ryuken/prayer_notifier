b368771663a8aeac1532c57d007f62fdb9d4a0ed
Taushif Habiboellah <taushif.habiboellah@gmail.com>
2019-08-14 18:04:34 +0200

    remove qm_remote

--- qm_remote/plugins/com.albahra.plugin.networkinterface/www/networkinterface.js
+++
@@ -1,8 +0,0 @@
-var networkinterface = function() {
-};
-
-networkinterface.getIPAddress = function(success, fail) {
-    cordova.exec(success, fail, "networkinterface", "getIPAddress", []);
-};
-
-module.exports = networkinterface;
