8dd422db1be4cb3d02f3902c264c213ab74790f2
Taushif Habiboellah <taushif.habiboellah@gmail.com>
2022-01-12 09:05:03 +0100

    update with pychromecast

--- daemon/check.go
+++ daemon/check.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"os/exec"
-	"regexp"
-	"time"
 	"net/http"
+	"os/exec"
+	"time"
 
 	"github.com/spf13/viper"
 )
@@ -27,15 +25,16 @@ func check() {
 
 		for _, prayer := range enabled {
 			if "Fajr" == prayer {
-				play("mpc", "play", "1")
+				play("python3", "/root/pychromecast-10.2.3/play.py", "azan12.mp3")
 				break
 			}
 		}
 	} else if currentTime == Today.Sunrise {
+		fmt.Println("It's Sunrise!")
+
 		for _, prayer := range enabled {
 			if "Sunrise" == prayer {
-				fmt.Println("It's Sunrise!")
-				play("mpc", "play", "2")
+				play("python3", "/root/pychromecast-10.2.3/play.py", "bleep.mp3")
 				break
 			}
 		}
@@ -44,7 +43,7 @@ func check() {
 
 		for _, prayer := range enabled {
 			if "Dhuhr" == prayer {
-				play("mpc", "play", "1")
+				play("python3", "/root/pychromecast-10.2.3/play.py", "azan12.mp3")
 				break
 			}
 		}
@@ -53,7 +52,7 @@ func check() {
 
 		for _, prayer := range enabled {
 			if "Asr" == prayer {
-				play("mpc", "play", "1")
+				play("python3", "/root/pychromecast-10.2.3/play.py", "azan12.mp3")
 				break
 			}
 		}
@@ -62,7 +61,7 @@ func check() {
 
 		for _, prayer := range enabled {
 			if "Maghrib" == prayer {
-				play("mpc", "play", "1")
+				play("python3", "/root/pychromecast-10.2.3/play.py", "azan12.mp3")
 				break
 			}
 		}
@@ -71,7 +70,7 @@ func check() {
 
 		for _, prayer := range enabled {
 			if "Isha" == prayer {
-				play("mpc", "play", "1")
+				play("python3", "/root/pychromecast-10.2.3/play.py", "azan12.mp3")
 				break
 			}
 		}
@@ -80,119 +79,15 @@ func check() {
 
 func play(player, action, id string) {
 
-	cmd := exec.Command("mpc", "playlist")
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	cmd := exec.Command(player, action, id)
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	var status []string
-
-	//fmt.Println(out.String())
-	//fmt.Println("Current playlist is not azan.playlist")
-
-	// get current playing status
-	cmd = exec.Command("mpc", "status")
-	cmd.Stdout = &out
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-	// Print status
-	re := regexp.MustCompile(`\[(\w+)\] #(\d+)/(\w+)\s+(\w+:\w+)/(\w+:\w+)`)
-	//fmt.Printf("%q\n", re.FindStringSubmatch("[playing] #1/1 0:05/27:02"))
-	status = re.FindStringSubmatch(fmt.Sprintf("%q\n", out.String()))
-	//fmt.Println(status)
-
-	if len(status) > 0 {
-		// remove temporary playlist
-		cmd = exec.Command("mpc", "rm", "temp.playlist")
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println("Removed temporary playlist")
-
-		// save playlist as temporary playlist
-		cmd = exec.Command("mpc", "save", "temp.playlist")
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		//fmt.Println("Saved current playlist to temp.playlist")
-
-		// clear current playlist
-		cmd = exec.Command("mpc", "clear")
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println("Cleared playlist")
-	}
-
-	// load azan playlist
-	cmd = exec.Command("mpc", "load", "azan.playlist")
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	//fmt.Println("Loaded azan.playlist")
-
-	cmd = exec.Command(player, action, id)
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	//fmt.Println("playing first from playlist and sleeping 3:21 mintues")
-
-	time.Sleep(3*time.Minute + 21*time.Second)
-
-	// stop playing
-	stop()
-	//fmt.Println("Stopped playing azan")
-
-	// clear temporary playlist
-	cmd = exec.Command("mpc", "clear")
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-	//fmt.Println("Cleared playlist")
-
-	if len(status) > 0 {
-		// load temp playlist
-		cmd = exec.Command("mpc", "load", "temp.playlist")
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println("Loaded temp.playlist again")
-
-		// resume  playlist
-		cmd = exec.Command("mpc", "play", status[2])
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println("Resumed original playlist")
-
-		// seek to original position
-		cmd = exec.Command("mpc", "seek", status[4])
-		err = cmd.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println("Seeked to original position")
-	}
 }
 
 // stop playing
-func stop() (error) {
+func stop() error {
 
 	cmd := exec.Command("mpc", "stop")
 	err := cmd.Start()
---
+++ daemon/scripts/play.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import pychromecast
+
+# List chromecasts on the network, but don't connect
+services, browser = pychromecast.discovery.discover_chromecasts()
+# Shut down discovery
+pychromecast.discovery.stop_discovery(browser)
+
+# Discover and connect to chromecasts named Living Room
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=["Living Room speaker"])
+[cc.cast_info.friendly_name for cc in chromecasts]
+['Living Room speaker']
+
+cast = chromecasts[0]
+# Start worker thread and wait for cast device to be ready
+cast.wait()
+print(cast.cast_info)
+
+mc = cast.media_controller
+
+mc.play_media('http://192.168.8.7/' + sys.argv[1], 'audio/mp3')
+mc.block_until_active()
+print(mc.status)
